b5951f2d976a9ae4641dcd35b6a78d3f9d78fa8e
thebigsmileXD <thebigsmilexd@gmail.com>
2017-02-03 11:02:11 +0100

    Update ClientboundMapItemDataPacket Thanks to @MrPowerGamerBR for linking https://github.com/PocketDreams/SequinLand/blob/master/src/main/java/cn/nukkit/network/protocol/ClientboundMapItemDataPacket.java

--- src/pocketmine/network/protocol/ClientboundMapItemDataPacket.php
+++ src/pocketmine/network/protocol/ClientboundMapItemDataPacket.php
@@ -1,44 +1,53 @@
 <?php
 namespace pocketmine\network\protocol;
-#include <rules/DataPacket.h>
-class ClientboundMapItemDataPacket extends DataPacket{
+class ClientboundMapItemDataPacket extends DataPacket {
 	const NETWORK_ID = Info::CLIENTBOUND_MAP_ITEM_DATA_PACKET;//0x41
+	const UPDATE = 6;
+	const FULL = 4;
+	
+	public $mapId; // VarLong
+	public $unknown; // Unsigned VarInt
+	public $unknown2; // Unsigned VarInt
+	public $unknown3; // VarLong
+	public $action; // Unsigned byte
+	public $unknown4; // Unsigned VarInt
+	public $unknown5; // VarInt
+	public $unknown6 = 1; // $~ https://github.com/NiclasOlofsson/MiNET/blob/5064e6d15778f9bfc5dae9fef30eeca10f02f887/src/MiNET/MiNET/Net/Package.cs#L1186
+	public $unknown7 = 0; // Byte
+	public $showIcons; // Boolean
+	public $icons = 0; // varint<xz>[] TODO: Add icons
+	public $direction; // VarInt
+	public $x; // varint<xz>
+	public $z; // ^^^
+	public $columns; // VarInt
+	public $rows; // VarInt
+	public $offsetX; // varint<xz>
+	public $offsetZ; // ^^^
+	public $data = []; // ubyte[]
 
-	public $eid1 = 0;//white dot?
-	public $varint1 = 5;//x-offset?
-	public $varint2 = 5;//z-offset?
-	public $eid2 = 0;//? maybe an array of markers positions
-	public $varint3 = 2;//rotation of entity 0-8? (360/8) = one step rotate
-	//over all.. some MapDecoration object is in IDA
-	public function decode(){
-		/*
-		$this->eid = $this->getEntityId();
-		$this->action = $this->getVarInt();
-		$this->getBlockCoords($this->x, $this->y, $this->z);
-		$this->face = $this->getVarInt();//still interact packet shit. needed the functions
-		*/
-		/*
-		getVarInt
-		getbyte
-		getbyte
-		getstring
-		getunsingedint
-		LATER:
-		getvarint
-		getvarint
-		getvarint
-		getvarint
-		LATER:
-		getunsingedvarint
-		BUT: 9 reads in total??
-		*/
+
+	public function decode() {
 	}
-	public function encode(){
-		$this->reset();
-		$this->putEntityId($this->eid1);
-		$this->putVarInt($this->varint1);
-		$this->putVarInt($this->varint2);
-		$this->putEntityId($this->eid2);
-		$this->putVarInt($this->varint3);
+
+	public function encode() {
+		$this->putLong($this->mapId);
+		$this->putUnsignedVarInt($this->unknown);
+		$this->putUnsignedVarInt($this->unknown2);
+		$this->putLong($this->unknown3);
+		$this->putByte($this->action);
+		$this->putUnsignedVarInt($this->unknown4);
+		$this->putVarInt($this->unknown5);
+		$this->putByte($this->unknown6);
+		$this->putByte($this->unknown7);
+		$this->putBool($this->showIcons);
+		$this->putUnsignedVarInt($this->icons); // No icons yet
+		$this->putVarInt($this->direction);
+		$this->putVarInt($this->x);
+		$this->putVarInt($this->z);
+		$this->putVarInt($this->columns);
+		$this->putVarInt($this->rows);
+		$this->putVarInt($this->offsetX);
+		$this->putVarInt($this->offsetZ);
+		$this->putDataArray($this->data);
 	}
 }
